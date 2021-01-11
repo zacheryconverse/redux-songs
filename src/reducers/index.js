49e7c2a1699271e4ed7d5e40c6834e018ef1b646
Zachery Converse <zacheryconverse@gmail.com>
2021-01-11 10:54:20 -0700

    Create reducers, and actions.

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:25' },
+    { title: 'All Star', duration: '3:03' },
+    { title: 'I Want it That Way', duration: '1:42' },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') return action.payload;
+  return selectedSong;
+};
