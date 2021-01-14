a0ba757ed297614d552cdb7a374fea9d7917732d
Zachery Converse <zacheryconverse@gmail.com>
2021-01-13 20:01:15 -0700

    Build SongList component. Hook up combineReducers, createStore, and Provider.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-render(<App/>, document.getElementById('root'));
+import App from './components/App';
+import reducers from './reducers';
+
+render(
+  <Provider store={createStore(reducers)}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,3 +13,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === 'SONG_SELECTED') return action.payload;
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
